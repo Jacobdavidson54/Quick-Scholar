5f4345b6f0ab23156c33935b776cc675bde382ec
Jacob Musa Mahlangu <nergtonicsa@gmail.com>
2026-03-29 16:35:22 +0200

    Removed file creation feature since database now handles data storage

--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,7 @@
 # utils.py
 import unicodedata
-import json
-import os
 import re
 
-FILE_NAME = "results_research.json"
-
-def initialize_file():
-    if not os.path.exists(FILE_NAME):
-        with open(FILE_NAME, "w", encoding="utf-8") as file:
-            json.dump([], file)
-
-def load_file():
-    try:
-        with open(FILE_NAME, "r", encoding="utf-8") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
-
-def save_data(data):
-    with open(FILE_NAME, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
 
 
 # normalize a single string
