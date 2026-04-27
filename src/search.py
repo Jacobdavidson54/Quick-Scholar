78dbe5ff3af7b6dca209654d5e53894610008e6b
Jacob Musa Mahlangu <nergtonicsa@gmail.com>
2026-04-27 11:26:45 +0200

    Integrated a new normalization function into the search logic to standardize API results.

--- src/search.py
+++ src/search.py
@@ -1,13 +1,9 @@
 # search.py
 from src.cache import get_cache, set_cache
 from src.db import get_papers_by_query, save_papers, insert_search_history
-from src.fetch import  fetch_all_sources
-from src.utils import  normalize_and_score, normalize_results
-# Bug fix : removed the old search logic and replaced it with a new function that calls fetch_all_sources to get data from all sources, then normalizes the results using normalize_results before returning them. This ensures that the search function is streamlined and leverages the improvements made in fetch.py and utils.py for better performance and cleaner code.
+from src.fetch import fetch_all_sources
+from src.utils import sort_by_year, normalize_results
 
-#upgrade : Imported the fetch_all_sources function from fetch.py to handle fetching data from all sources in a single function. This simplifies the search_papers function and keeps the fetching logic centralized in fetch.py, making it easier to maintain and update in the future.
-
-# imported asyncio to run the asynchronous fetch_all_sources function within the search_papers function, allowing for efficient handling of multiple API calls without blocking the main thread.
 
 async def search_papers(query):
 
@@ -17,7 +13,7 @@ async def search_papers(query):
     if cached:
         print("Cache hit for query:", query)
         return cached
-    
+
     db_results = get_papers_by_query(query)
     if db_results:
         print("Database hit for query:", query)
@@ -28,14 +24,13 @@ async def search_papers(query):
     all_results = await fetch_all_sources(query)
 
     if not all_results:
-        return {"error": "No papers found"}
+        return []
 
-# replaced the old normalization logic with a call to normalize_and_score, which not only normalizes the results but also scores them based on relevance to the query. This provides a more comprehensive processing of the results before they are returned to the user.
+    
     all_results = normalize_results(all_results)
-    all_results = normalize_and_score(all_results, query)
+    all_results = sort_by_year(all_results)
 
     set_cache(query, all_results)
-    save_papers(query, all_results) 
+    save_papers(query, all_results)
 
-
-    return all_results, 200
+    return all_results
